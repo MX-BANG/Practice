0274da735e06a63fbc374e346303ff7a1a65ac32
Moiz Ali Siddiqui <siddiqui.moizali1@gmail.com>
2025-06-11 12:46:00 +0500

    refactor script.js to improve variable naming and enhance greeting functionality

--- Day_3(Form Handling)/script.js
+++ Day_3(Form Handling)/script.js
@@ -1,11 +1,13 @@
-const input1 = document.getElementById("input");
-const btn = document.getElementById("Submitbtn");
-const greeting = document.getElementById("greetings")
+const textInput = document.getElementById("textinput");
+const colorInput = document.getElementById("colorinput");
+const btn = document.getElementById("btn");
+const greetings = document.getElementById("greetings");
 
-btn.addEventListener("click", function () {
-    const userText = input1.value;
-    greeting.innerText = "Welcome " + userText
-    greeting.style.color = "red";
-    greeting.style.fontSize = "24px";
-});
+btn.addEventListener("click", function(){
+    const UserInput = textInput.value;
+    const color = colorInput.value;
 
+    greetings.innerText = "Welcome " + UserInput;
+    greetings.style.color = color;
+
+})
